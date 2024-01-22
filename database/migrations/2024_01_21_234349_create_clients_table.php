872fadb23a837f9c12ffc551d4f85e8ed3bb0d78
brunovt074 <brunodev.pdl@gmail.com>
2024-01-22 12:05:20 -0300

    user seeder updated

--- database/migrations/2024_01_21_234349_create_clients_table.php
+++ database/migrations/2024_01_21_234349_create_clients_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('logo_url');
+            $table->string('name');                        
             $table->timestamps();
         });
     }
