c1cfa2bae27492aefb506bf6028754d6682f0f12
brunovt074 <brunodev.pdl@gmail.com>
2024-01-23 00:23:25 -0300

    added index.blade, app.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,5 +3,32 @@
 @section('title', 'Portfolio')
 
 @section('content')
-div
+<div class='container pt-4 pb-4 index-title'></div>
+<div class='container pt-4 index-subtitle'> 
+    <h1 class=''><strong>Bruno Vargas Tettamanti</strong></h1>
+    <h2 class=''>Software Developer</h2>
+</div>
+<!-- <section class='about-me'>
+
+</section>
+
+<section class='projects'>
+
+</section>
+
+<section class='stack'>
+
+</section>
+
+<section class='education'>
+
+</section>
+
+<section class='hobbies'>
+
+</section>
+
+<section class='contact'>
+
+</section> -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap CSS -->
-    @section
+    <!-- Bootstrap CSS -->    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    @endsection
+    <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/app.css') }}">
+    
 
     <title>@yield('title')</title>
 </head>
 <body>
-    @section('navbar')
+    
     <header>
-
-
-    </header>
-    @endsection
+        <nav class="navbar sticky-top navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Sticky</a>
+            </div>
+        </nav>
+    </header>  
     
     <main>
         <div>
